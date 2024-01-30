101c47cff4942ea3a122c95689f20acc68073784
GauravShreenet <gthakuri656@gmail.com>
2024-01-30 22:36:44 +1100

    fetched the category from the database

--- src/router/fetchRouter.js
+++ src/router/fetchRouter.js
@@ -17,13 +17,32 @@ router.get("/new-arrival", async(req, res, next)=> {
 
         const newArrive = await colName.find({}).sort({ createdAt: -1 }).limit(12).toArray()
 
-        console.log(newArrive);
         return responder.SUCCESS({
             res,
             message: "Here are your latest arrival",
             newArrive
         })
     
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/categories", async(req, res, next)=> {
+    try {
+        await client.connect();
+
+        const dbName = client.db('fashon');
+        const colName = dbName.collection('categories')
+
+        const categories = await colName.find({}).toArray()
+
+        return responder.SUCCESS({
+            res,
+            message: "Here are the Categories",
+            categories
+        })
+
     } catch (error) {
         next(error)
     }
